ff9402787b9821818895123b8a023b72ec2496c9
Oliver Hertzberg <oliverhertzberg@hotmail.com>
2023-02-24 20:26:36 +0200

    tehtävä2.8

--- src/App.js
+++ src/App.js
@@ -3,43 +3,58 @@ import {useState} from 'react'
 const App = () => {
   const [persons, setPersons] = useState([{
     name: 'Arto Hellas',
+    number: '040-1231244'
   }])
   const [newName, setNewName] = useState('')
+  const [newNum, setNewNum] = useState('')
 
   const handleNameChange = (e) => {
     console.log(e.target.value)
     setNewName(e.target.value)
   }
+  const handleNumChange = (e) => {
+    console.log(e.target.value)
+    setNewNum(e.target.value)
+  }
 
-  const addName = (e) => {
-    console.log('button clicked')
+  const addPerson = (e) => {
     e.preventDefault()
     const nameCheck = persons.map(person => person.name);
-      return nameCheck.includes(newName)
-        ? alert(`The name "${newName}" is already in use, please type in another name.`)
-        : (
-          setPersons(persons.concat({
-            name: newName,
-          })),
-          setNewName('')
-        )
+    const numCheck = persons.map(person => person.number);
+
+    if (nameCheck.includes(newName)) {
+      alert(`The name "${newName}" is already in use, please type in another name.`)
+    } else if (numCheck.includes(newNum)) {
+      alert(`The number "${newNum}" is already in use, please type in another one.`)
+    } else {
+      setPersons(persons.concat({
+        name: newName,
+        number: newNum,
+      }))
+      setNewName('')
+      setNewNum('')
+    }
+
   }
   
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          <button type='submit' onClick={addName}>Add</button>
+          number: <input value={newNum} onChange={handleNumChange} />
+        </div>
+        <div>
+          <button type='submit' onClick={addPerson}>Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+        {persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
       </ul>
     </div>
   )
