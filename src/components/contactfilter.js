ccfb40fbdc845e0c162c07e9720ffdc46a01d9ba
Oliver Hertzberg <oliverhertzberg@hotmail.com>
2023-04-11 17:15:40 +0300

    tehtävä2_18

--- src/components/contactfilter.js
+++
@@ -1,7 +0,0 @@
-const Filter = (props) => {
-    return(<div>
-      filter shown with <input type="search" value={props.query}  onChange={e => props.setQuery(e.target.value)} />
-    </div>)
-}
-
-export default Filter
